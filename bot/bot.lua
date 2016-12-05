a6f67af59b42d654a8d33bf44f1cf693067b81d6
COINHA <coinha7@gmail.com>
2016-12-05 16:45:38 +0100

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {169257988},
+    sudo_users = {224519130},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
